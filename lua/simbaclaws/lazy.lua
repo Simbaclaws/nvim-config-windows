dd63bdfe0d960bce58bc4198a26dbe320a155fc8
Simbaclaws <hylke.hellinga@gmail.com>
2024-09-26 23:05:20 +0200

    Mistake in lazy.lua

--- lua/simbaclaws/lazy.lua
+++ lua/simbaclaws/lazy.lua
@@ -22,7 +22,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
-require("lazy").setup({ import = {"simbaclaws.plugins"}, { import = "simbaclaws.plugins.lsp"}},{
+require("lazy").setup({ { import = "simbaclaws.plugins"}, { import = "simbaclaws.plugins.lsp"} },{
   checker = {
     enabled = true,
     notify = false,
